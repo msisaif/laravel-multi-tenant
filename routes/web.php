787a9665ae6014d7136fab411e2b2b4a0149d10d
msisaif <msi132043@gmail.com>
2023-09-01 19:04:05 +0600

    multi tenant by spatie/laravel-multitenancy package

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::domain('localhost')->group(function(){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return app('currentTenant');
 });
